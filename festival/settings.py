c1e3c42bda19602a4b872875c7fcbb85533a76ed
Artem Galkin <igalart2000@gmail.com>
2022-03-24 14:33:01 +0300

    raw

--- festival/settings.py
+++ festival/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'main_app/static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
